b2b38ff5eeb2fead414e3d1afcf7c11d49f62426
Vitor Soares <vitorsoaresf@gmail.com>
2024-03-14 07:54:05 -0300

    feat: implements for from user list

--- learning-angular/src/app/pages/home/home.component.ts
+++ learning-angular/src/app/pages/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { UserCardComponent } from '../../components/user-card/user-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [UserCardComponent],
+  imports: [CommonModule, UserCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   messageHome = 'Home';
-  userA = { name: 'Alex', email: 'alex@mail.com', job: 'instructor' };
-  userB = { name: 'Joao', email: 'joao@mail.com', job: 'developer' };
+  userList = [
+    { name: 'Alex', email: 'alex@mail.com', job: 'instructor' },
+    { name: 'Joao', email: 'joao@mail.com', job: 'developer' },
+  ];
 }
